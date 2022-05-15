94d5fcbc92d277a9e1480d8f0006b092217820b4
Yena-Yun <cheryleduart@gmail.com>
2022-05-15 21:52:30 +0900

    :sparkles::memo: 1-5 문제풀이 및 BEFORE, AFTER 작성

--- 1. 기본/05. 최솟값 구하기/main.js
+++ 1. 기본/05. 최솟값 구하기/main.js
@@ -1,5 +1,15 @@
 function solution(arr) {
-  return answer;
+  let min = Number.MAX_SAFE_INTEGER;
+
+  if (arr.length !== 7) {
+    return;
+  } else {
+    arr.forEach((item) => {
+      if (item < min) min = item;
+    });
+  }
+
+  return min;
 }
 
 let arr = [5, 7, 1, 3, 2, 9, 11];
