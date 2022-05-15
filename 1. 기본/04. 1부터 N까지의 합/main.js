77b8647b28916ed1bde7dac21e1bd1d626099571
Yena-Yun <cheryleduart@gmail.com>
2022-05-15 21:41:32 +0900

    :sparkles::memo: 1-4 문제풀이 및 BEFORE, AFTER 작성

--- 1. 기본/04. 1부터 N까지의 합/main.js
+++ 1. 기본/04. 1부터 N까지의 합/main.js
@@ -1,5 +1,15 @@
 function solution(n) {
-  return answer;
+  let sum = 0;
+
+  if (n > 20) {
+    return;
+  } else {
+    for (let i = 1; i <= n; i++) {
+      sum += i;
+    }
+  }
+
+  return sum;
 }
 
 console.log(solution(10));
