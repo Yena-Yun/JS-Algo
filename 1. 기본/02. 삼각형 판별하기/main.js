26ce29bcbc6dd86308bd1fbd4f84c3c7f0d478f2
Yena-Yun <cheryleduart@gmail.com>
2022-05-15 21:24:36 +0900

    :sparkles::memo: 1-2 문제풀이, AFTER 작성

--- 1. 기본/02. 삼각형 판별하기/main.js
+++ 1. 기본/02. 삼각형 판별하기/main.js
@@ -1,4 +1,13 @@
 function solution(a, b, c) {
+  let answer;
+  let max = a;
+
+  if (b > max) max = b;
+  if (c > max) max = c;
+
+  if (max > a + b + c - max) answer = 'YES';
+  else answer = 'NO';
+
   return answer;
 }
 
