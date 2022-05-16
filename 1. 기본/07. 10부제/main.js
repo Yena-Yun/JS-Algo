802cd03a9b8d05c4286ca48c891d42f33aa51547
Yena-Yun <cheryleduart@gmail.com>
2022-05-16 22:07:08 +0900

    :sparkles::memo: 1-7 문제풀이, logic 작성

--- 1. 기본/07. 10부제/main.js
+++ 1. 기본/07. 10부제/main.js
@@ -1,6 +1,25 @@
 function solution(day, arr) {
-  return answer;
+  let answer = [];
+
+  for (let x of arr) {
+    if (x % 10 === day) answer.push(x);
+  }
+
+  return answer.length;
 }
 
 arr = [25, 23, 11, 47, 53, 17, 33];
 console.log(solution(3, arr));
+
+function solution(day, arr) {
+  let answer = [];
+
+  for (let x of arr) {
+    if (Number(x.toString()[1]) === day) answer.push(x);
+  }
+
+  return answer.length;
+}
+
+array = [25, 23, 11, 47, 53, 17, 33];
+console.log(solution(3, array));
