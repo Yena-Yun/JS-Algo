11f591de2c74889b930d5b59064f49aa1b7f1246
Yena-Yun <cheryleduart@gmail.com>
2022-05-16 09:03:04 +0900

    :sparkles::memo: 1-6 문제풀이 및 BEFORE, AFTER 작성

--- 1. 기본/06. 홀수/main.js
+++ 1. 기본/06. 홀수/main.js
@@ -1,4 +1,23 @@
 function solution(arr) {
+  let answer = [];
+  let oddArr = [];
+  let sum = 0;
+  let min = Number.MAX_SAFE_INTEGER;
+
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      oddArr.push(x);
+    }
+  }
+
+  sum = oddArr.reduce((acc, cur) => acc + cur, 0);
+
+  for (let x of oddArr) {
+    if (x < min) min = x;
+  }
+
+  answer.push(sum, min);
+
   return answer;
 }
 
