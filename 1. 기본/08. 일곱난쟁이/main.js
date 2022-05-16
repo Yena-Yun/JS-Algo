e047c1cda3fa94aa4e4df9f28eaf0892b16a9901
Yena-Yun <cheryleduart@gmail.com>
2022-05-17 08:57:55 +0900

    :sparkles::memo: 1-8 문제풀이 및 BEFORE, AFTER 작성

--- 1. 기본/08. 일곱난쟁이/main.js
+++ 1. 기본/08. 일곱난쟁이/main.js
@@ -1,4 +1,34 @@
 function solution(arr) {
+  let answer = [];
+  let sum = 0;
+  let firstIndex;
+  let secondIndex;
+
+  for (let x of arr) {
+    sum += x;
+  }
+
+  let extraSum = sum - 100;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === extraSum) {
+        firstIndex = arr.indexOf(arr[i]);
+        secondIndex = arr.indexOf(arr[j]);
+
+        console.log(firstIndex);
+        console.log(secondIndex);
+      }
+    }
+  }
+
+  for (let x of arr) {
+    if (x === arr[firstIndex] || x === arr[secondIndex]) {
+      continue;
+    }
+    answer.push(x);
+  }
+
   return answer;
 }
 
