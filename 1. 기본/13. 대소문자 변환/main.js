c8de118aafa28eb052266bf784d4213534a23f64
Yena-Yun <cheryleduart@gmail.com>
2022-05-24 21:37:54 +0900

    :sparkles: 1-13 문제풀이 및 BEFORE, AFTER 작성

--- 1. 기본/13. 대소문자 변환/main.js
+++ 1. 기본/13. 대소문자 변환/main.js
@@ -1,5 +1,12 @@
 function solution(s) {
-  return answer;
+  let answer = [];
+
+  for (let x of s) {
+    if (x === x.toUpperCase()) answer.push(x.toLowerCase());
+    else answer.push(x.toUpperCase());
+  }
+
+  return answer.join('');
 }
 
 console.log(solution('StuDY'));
