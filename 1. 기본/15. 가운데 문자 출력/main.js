e8c4448b036df14a877f73842f7e24a6c6241ce0
Yena-Yun <cheryleduart@gmail.com>
2022-05-25 08:54:43 +0900

    :sparkles::memo: 1-15 문제풀이 및 BEFORE, AFTER 로직 작성

--- 1. 기본/15. 가운데 문자 출력/main.js
+++ 1. 기본/15. 가운데 문자 출력/main.js
@@ -1,4 +1,13 @@
 function solution(s) {
+  let answer;
+  let targetIndex = Math.floor(s.length / 2);
+
+  if (s.length % 2 === 1) {
+    answer = s[targetIndex];
+  } else if (s.length % 2 === 0) {
+    answer = s[targetIndex - 1] + s[targetIndex];
+  }
+
   return answer;
 }
 console.log(solution('study'));
