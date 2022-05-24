516e6a60190a85e08afb543fe563adce2171942b
Yena-Yun <cheryleduart@gmail.com>
2022-05-24 21:54:19 +0900

    :sparkles: 1-14 문제풀이 및  BEFORE, AFTER 작성

--- 1. 기본/14. 가장 긴 문자열/main.js
+++ 1. 기본/14. 가장 긴 문자열/main.js
@@ -1,4 +1,14 @@
 function solution(s) {
+  let answer;
+  let maxLength = Number.MIN_SAFE_INTEGER;
+
+  for (let x of s) {
+    if (x.length > maxLength) {
+      maxLength = x.length;
+      answer = x;
+    }
+  }
+
   return answer;
 }
 let str = ['teacher', 'time', 'student', 'beautiful', 'good'];
