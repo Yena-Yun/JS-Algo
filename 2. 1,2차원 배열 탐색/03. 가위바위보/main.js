3190adab07f78633ea3a60f821c5bf4e3244dda6
Yena-Yun <cheryleduart@gmail.com>
2022-05-25 22:47:31 +0900

    :memo::package: 챕터2 폴더구조 및 md 생성

---
+++ 2. 1,2차원 배열 탐색/03. 가위바위보/main.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+  return answer;
+}
+
+let a = [2, 3, 3, 1, 3];
+let b = [1, 1, 2, 2, 3];
+console.log(solution(a, b));
---
+++ 2. 1,2차원 배열 탐색/04. 점수 계산/main.js
@@ -0,0 +1,6 @@
+function solution(arr) {
+  return answer;
+}
+
+let arr = [1, 0, 1, 1, 1, 0, 0, 1, 1, 0];
+console.log(solution(arr));
