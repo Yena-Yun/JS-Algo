9e241445b53e60728d5131e31fb2f20886a7847c
Yena-Yun <cheryleduart@gmail.com>
2022-05-25 22:32:22 +0900

    :sparkles::memo::art: 1-17 문제풀이 및 BEFORE, AFTER 로직 작성 + 1-16과 1-17 다른 방법으로 추가 풀이

--- 1. 기본/17. 중복단어 제거/main.js
+++ 1. 기본/17. 중복단어 제거/main.js
@@ -1,5 +1,21 @@
 function solution(s) {
+  let answer = [];
+
+  for (let x of s) {
+    if (!answer.includes(x)) answer.push(x);
+  }
+
   return answer;
 }
 let str = ['good', 'time', 'good', 'time', 'student'];
 console.log(solution(str));
+
+// --------------------------------------------------------
+
+function solution(s) {
+  let answer = s.filter((v, i) => s.indexOf(v) === i);
+
+  return answer;
+}
+let string = ['good', 'time', 'good', 'time', 'student'];
+console.log(solution(string));
