3a1a9861613140efc3f20be2eff62b58150287f9
Yena-Yun <cheryleduart@gmail.com>
2022-05-28 12:28:30 +0900

    :sparkles::memo: 2-2 문제풀이 및 BEFORE, AFTER 로직 작성

--- 2. 1,2차원 배열 탐색/02. 보이는 학생/main.js
+++ 2. 1,2차원 배열 탐색/02. 보이는 학생/main.js
@@ -1,5 +1,16 @@
 function solution(arr) {
-  return answer;
+  let answer = [];
+  let max = arr[0];
+
+  answer.push(arr[0]);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      answer.push(arr[i]);
+      max = arr[i];
+    }
+  }
+
+  return answer.length;
 }
 
 let arr = [130, 135, 148, 140, 145, 150, 150, 153];
