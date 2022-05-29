7d599bfe27b95dd89f94046452615645b768bf04
Yena-Yun <cheryleduart@gmail.com>
2022-05-29 12:53:29 +0900

    :sparkles::memo: 2-4 문제풀이 및 BEFORE, AFTER 로직 작성

--- 2. 1,2차원 배열 탐색/04. 점수 계산/main.js
+++ 2. 1,2차원 배열 탐색/04. 점수 계산/main.js
@@ -1,4 +1,15 @@
 function solution(arr) {
+  let answer = 0; // 최종 점수 총합(sum)
+  let accumulate = 0; // 가산점이 반영된 점수
+
+  for (let x of arr) {
+    if (x === 0) accumulate = 0;
+    else {
+      accumulate += 1;
+      answer += accumulate;
+    }
+  }
+
   return answer;
 }
 
--- 2. 1,2차원 배열 탐색/05. 등수 구하기/main.js
+++ 2. 1,2차원 배열 탐색/05. 등수 구하기/main.js
@@ -1,6 +1,15 @@
 function solution(arr) {
+  let n = arr.length;
+  let answer = Array.from({ length: n }, () => 1);
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (arr[i] > arr[j]) answer[j]++;
+    }
+  }
+
   return answer;
 }
 
-let arr = [87, 89, 92, 100, 76];
+let arr = [74, 89, 92, 92, 100, 92];
 console.log(solution(arr));
