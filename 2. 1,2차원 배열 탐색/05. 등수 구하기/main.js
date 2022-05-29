28a0093923c92615d70a35528391029f621754ed
Yena-Yun <cheryleduart@gmail.com>
2022-05-29 15:32:22 +0900

    :sparkles::memo: 2-5 문제풀이 및 BEFORE, AFTER 작성

--- 2. 1,2차원 배열 탐색/05. 등수 구하기/main.js
+++ 2. 1,2차원 배열 탐색/05. 등수 구하기/main.js
@@ -1,6 +1,6 @@
 function solution(arr) {
-  let n = arr.length;
   let answer = Array.from({ length: n }, () => 1);
+  let n = arr.length;
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
