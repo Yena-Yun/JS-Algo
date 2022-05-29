110fe38755d10070be9483dadf6d0fe319cd48da
Yena-Yun <cheryleduart@gmail.com>
2022-05-29 15:28:56 +0900

    :sparkles::memo: 2-6 문제풀이 및 BEFORE, AFTER 작성

--- 2. 1,2차원 배열 탐색/06. 격자판 최대합/main.js
+++ 2. 1,2차원 배열 탐색/06. 격자판 최대합/main.js
@@ -1,4 +1,30 @@
 function solution(arr) {
+  let answer = Number.MIN_SAFE_INTEGER;
+  let n = arr.length;
+  let sum1 = 0; // 행의 합 또는 첫번째 대각선의 합
+  let sum2 = 0; // 열의 합 또는 두번째 대각선의 합
+
+  // 각 행의 합과 각 열의 합 중 최대값 구하기
+  for (let i = 0; i < n; i++) {
+    sum1 = sum2 = 0;
+
+    for (let j = 0; j < n; j++) {
+      sum1 += arr[i][j];
+      sum2 += arr[j][i];
+    }
+    answer = Math.max(answer, sum1, sum2);
+  }
+
+  // 변수를 재활용하기 위해 0으로 초기화
+  sum1 = sum2 = 0;
+
+  // 두 대각선의 합 중 최대값 구하기
+  for (let i = 0; i < n; i++) {
+    sum1 += arr[i][i];
+    sum2 += arr[i][n - i - 1];
+  }
+  answer = Math.max(answer, sum1, sum2);
+
   return answer;
 }
 
