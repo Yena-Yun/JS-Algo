a6077d4ebceb4c37dad69774def993cd0db585d0
Yena-Yun <cheryleduart@gmail.com>
2022-05-30 23:46:35 +0900

    :construction::memo: 2-7 BEFORE 로직 작성 및 문제풀이 중

--- 2. 1,2차원 배열 탐색/07. 봉우리/main.js
+++ 2. 1,2차원 배열 탐색/07. 봉우리/main.js
@@ -1,4 +1,40 @@
 function solution(arr) {
+  let answer = 0;
+  // 각 배열값의 동서남북 좌표
+  let nx;
+  let ny;
+  let mx;
+  let my;
+  let coord = [];
+  let cnt = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      nx = arr[i][j + 1];
+      // ny = arr[i - 1][j];
+      mx = arr[i][j - 1];
+      // my = arr[i + 1][j];
+
+      console.log(nx, ny, mx, my);
+
+      // if (nx === null) nx = 0;
+      // else if (ny === null) ny = 0;
+      // else if (mx === null) mx = 0;
+      // else if (my === null) my = 0;
+
+      // coord.push(nx, ny, mx, my);
+
+      // for (let k = 0; k < 4; k++) {
+      //   if (coord[k] < arr[i][j]) cnt++;
+      // }
+
+      // if (cnt === 4) {
+      //   answer++;
+      //   cnt = 0;
+      // }
+    }
+  }
+
   return answer;
 }
 
