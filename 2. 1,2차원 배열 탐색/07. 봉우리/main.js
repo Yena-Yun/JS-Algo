a3c111df6fa2d6fd4641f5ed578fbae245cbab6a
Yena-Yun <cheryleduart@gmail.com>
2022-05-31 22:25:27 +0900

    :construction::memo: 2-7 문제풀이 및 로직 작성중

--- 2. 1,2차원 배열 탐색/07. 봉우리/main.js
+++ 2. 1,2차원 배열 탐색/07. 봉우리/main.js
@@ -1,37 +1,31 @@
 function solution(arr) {
   let answer = 0;
-  // 각 배열값의 동서남북 좌표
   let nx;
   let ny;
   let mx;
   let my;
-  let coord = [];
-  let cnt = 0;
+  let coord;
+  let cnt = 0; // 봉우리인지 판별하기 위한 카운트 (4가 되면 해당 arr[i][j]값은 봉우리)
 
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
-      nx = arr[i][j + 1];
-      // ny = arr[i - 1][j];
-      mx = arr[i][j - 1];
-      // my = arr[i + 1][j];
+      // 각 배열값의 동서남북 좌표
+      nx = i - 1 >= 0 ? arr[i - 1][j] : 0;
+      ny = j + 1 < arr.length ? arr[i][j + 1] : 0;
+      mx = i + 1 < arr.length ? arr[i + 1][j] : 0;
+      my = j - 1 >= 0 ? arr[i][j - 1] : 0;
+      coord = [nx, ny, mx, my];
 
-      console.log(nx, ny, mx, my);
+      for (let k = 0; k < 4; k++) {
+        if (coord[k] < arr[i][j]) {
+          cnt++;
+        }
 
-      // if (nx === null) nx = 0;
-      // else if (ny === null) ny = 0;
-      // else if (mx === null) mx = 0;
-      // else if (my === null) my = 0;
-
-      // coord.push(nx, ny, mx, my);
-
-      // for (let k = 0; k < 4; k++) {
-      //   if (coord[k] < arr[i][j]) cnt++;
-      // }
-
-      // if (cnt === 4) {
-      //   answer++;
-      //   cnt = 0;
-      // }
+        if (cnt === 4) {
+          answer++; // 봉우리 1개 추가
+          cnt = 0; // cnt 초기화
+        }
+      }
     }
   }
 
