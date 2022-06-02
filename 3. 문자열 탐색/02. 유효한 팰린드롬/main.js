05b76fdf4956d0bef7b2f7e057b74ee01dff044f
Yena-Yun <cheryleduart@gmail.com>
2022-06-02 10:19:35 +0900

    :sparkles::memo: 3-3 문제풀이 및 BEFORE, AFTER 작성

--- 3. 문자열 탐색/02. 유효한 팰린드롬/main.js
+++ 3. 문자열 탐색/02. 유효한 팰린드롬/main.js
@@ -1,5 +1,14 @@
 function solution(s) {
-  let answer;
+  let answer = 'YES';
+  // let reg = /[^A-Za-z]/gi; // 처음에 작성
+  let reg = /[^a-z]/g;
+  // let newStr = s.replace(reg, '').toLowerCase(); // 처음에 작성
+  let newStr = s.toLowerCase().replace(reg, '');
+  let len = newStr.length;
+
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (newStr[i] !== newStr[len - i - 1]) return 'NO';
+  }
 
   return answer;
 }
