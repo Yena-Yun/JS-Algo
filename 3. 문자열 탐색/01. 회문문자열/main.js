a7f7f99020b26d5e0ef51c2dee121e0ca991aa92
Yena-Yun <cheryleduart@gmail.com>
2022-06-02 09:59:05 +0900

    :sparkles::memo: 3-1 문제풀이 및 BEFORE, AFTER 작성

--- 3. 문자열 탐색/01. 회문문자열/main.js
+++ 3. 문자열 탐색/01. 회문문자열/main.js
@@ -1,8 +1,43 @@
+// function solution(s) {
+//   if (s.split('').reverse().join('').toLowerCase() === s.toLowerCase())
+//     return 'YES';
+//   else return 'NO';
+// }
+
+// let str = 'goooG';
+// console.log(solution(str));
+
+// -------------------------------------------------------
+
+// function solution(s) {
+//   let answer = 'NO';
+//   let newStr = [];
+
+//   for (let x of s) {
+//     newStr.push(x.toLowerCase());
+//   }
+
+//   if (s.toLowerCase() === newStr.reverse().join('')) answer = 'YES';
+
+//   return answer;
+// }
+
+// let string = 'goooG';
+// console.log(solution(string));
+
+// -------------------------------------------------------
+
 function solution(s) {
-  let answer;
+  let answer = 'YES';
+  s = s.toLowerCase();
+  let len = s.length;
+
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] !== s[len - i - 1]) return 'NO';
+  }
 
   return answer;
 }
 
-let str = 'goooG';
-console.log(solution(str));
+let string = 'goooG';
+console.log(solution(string));
