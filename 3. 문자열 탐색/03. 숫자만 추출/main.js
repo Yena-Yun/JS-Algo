d256e434ea462daa56cbac0108148f79c4e57787
Yena-Yun <cheryleduart@gmail.com>
2022-06-02 10:58:17 +0900

    :sparkles::memo: 3-3 문제풀이 및 BEFORE, AFTER 작성

--- 3. 문자열 탐색/03. 숫자만 추출/main.js
+++ 3. 문자열 탐색/03. 숫자만 추출/main.js
@@ -1,8 +1,25 @@
-function solution(str) {
-  let answer;
+// function solution(str) {
+//   let answer;
+//   let reg = /[^0-9]/g;
 
-  return answer;
+//   let result = str.replace(reg, '');
+//   answer = Number(result);
+
+//   return answer;
+// }
+
+// let str = 'g0en2T0s8eSoft';
+// console.log(solution(str));
+
+function solution(str) {
+  let answer = '';
+
+  for (let x of str) {
+    if (!isNaN(x)) answer += x;
+  }
+
+  return parseInt(answer);
 }
 
-let str = 'g0en2T0s8eSoft';
-console.log(solution(str));
+let string = 'g0en2T0s8eSoft';
+console.log(solution(string));
