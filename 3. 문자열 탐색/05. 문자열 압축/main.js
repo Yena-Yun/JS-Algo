42fc42a84b9ed9db2eba8695c1d75255ef27d4bd
Yena-Yun <cheryleduart@gmail.com>
2022-06-05 16:41:55 +0900

    :sparkles::memo: 3-5 문제풀이 및 BEFORE, AFTER 로직 작성

--- 3. 문자열 탐색/05. 문자열 압축/main.js
+++ 3. 문자열 탐색/05. 문자열 압축/main.js
@@ -1,8 +1,43 @@
+// function solution(s) {
+//   let answer = s[0];
+//   let target = s[0];
+//   let cnt = 0;
+
+//   for (let x of s) {
+//     if (target !== x) {
+//       if (cnt === 1) cnt = '';
+//       answer += cnt;
+//       cnt = 1;
+
+//       target = x;
+//       answer += x;
+//     } else cnt++;
+//   }
+
+//   return answer;
+// }
+
+// let str = 'KKHSSSSSSSE';
+// console.log(solution(str));
+
 function solution(s) {
-  let answer;
+  let answer = '';
+  let cnt = 1;
+  // s = s + ' ';
+
+  // for (let i = 0; i < s.length - 1; i++) {
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      cnt++;
+    } else {
+      answer += s[i];
+      if (cnt > 1) answer += String(cnt);
+      cnt = 1;
+    }
+  }
 
   return answer;
 }
 
-let str = 'KKHSSSSSSSE';
-console.log(solution(str));
+let string = 'KKHSSSSSSSE';
+console.log(solution(string));
