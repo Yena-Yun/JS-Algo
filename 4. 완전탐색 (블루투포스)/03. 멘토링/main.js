519eb0cac0a5772146fc6f4fe3d3ef042ad719df
Yena-Yun <cheryleduart@gmail.com>
2022-06-07 15:48:02 +0900

    :memo: 4-3 로직 작성

--- 4. 완전탐색 (블루투포스)/03. 멘토링/main.js
+++ 4. 완전탐색 (블루투포스)/03. 멘토링/main.js
@@ -1,5 +1,15 @@
 function solution(test) {
+  let m = test.length; // 총 테스트 횟수
+  let n = test[0].length; // 총 학생 수
   let answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= n; j++) {
+      let cnt = 0;
+
+      for (let k = 0; k < m; k++) {}
+    }
+  }
 
   return answer;
 }
