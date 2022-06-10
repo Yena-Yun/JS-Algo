7e65c10e379014594d102569389a8ddbebb9c86d
Yena-Yun <cheryleduart@gmail.com>
2022-06-10 13:08:44 +0900

    :sparkles::memo: 4-5 문제풀이 및 BEFORE, AFTER 로직 작성(완전탐색 드디어 끝git add .)

--- 4. 완전탐색 (블루투포스)/05. K번째 큰 수/main.js
+++ 4. 완전탐색 (블루투포스)/05. K번째 큰 수/main.js
@@ -1,6 +1,23 @@
 function solution(n, k, card) {
-  let answer;
+  let answer = 0;
+  // let sum = [];
   let tmp = new Set();
+
+  for (let i = 0; i < n; i++) {
+    for (let j = i + 1; j < n; j++) {
+      for (let k = j + 1; k < n; k++) {
+        // if (i !== j && j !== k && i !== k)
+        // sum.push(card[i] + card[j] + card[k]);
+        tmp.add(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  // let tmp = [...new Set(sum)];
+  // tmp.sort((a, b) => b - a);
+  // answer = tmp[k - 1];
+
+  let a = Array.from(tmp).sort((a, b) => b - a);
+  answer = a[k - 1];
 
   return answer;
 }
