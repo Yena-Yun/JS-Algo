ef81f1a9fcb53ca72dab89bcde06607074247e4b
Yena-Yun <cheryleduart@gmail.com>
2022-06-10 13:30:10 +0900

    :package: 챕터5 폴더구조 및 파일 생성

---
+++ 5. 효율성 (투포인터, 슬라이딩, 해쉬)/08. 모든 아나그램/main.js
@@ -0,0 +1,13 @@
+function compareMaps(map1, map2) {
+  return true;
+}
+function solution(s, t) {
+  let answer = 0;
+  let tH = new Map();
+  let sH = new Map();
+  return answer;
+}
+
+let a = 'bacaAacba';
+let b = 'abc';
+console.log(solution(a, b));
