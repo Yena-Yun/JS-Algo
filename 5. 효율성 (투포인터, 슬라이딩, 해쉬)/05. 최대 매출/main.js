4628fe0518eb5bb2a5b6b77dc4541573a4067584
Yena-Yun <cheryleduart@gmail.com>
2022-06-17 17:15:50 +0900

    :sparkles::memo: 5-5 문제풀이 및 BEFORE, AFTER 로직 작성

--- 5. 효율성 (투포인터, 슬라이딩, 해쉬)/05. 최대 매출/main.js
+++ 5. 효율성 (투포인터, 슬라이딩, 해쉬)/05. 최대 매출/main.js
@@ -1,7 +1,49 @@
+// 1회차 풀이
+// function solution(k, arr) {
+//   let answer = 0;
+//   let lt = 0;
+//   let sum = 0;
+//   let max = Number.MIN_SAFE_INTEGER;
+
+//   for (let rt = 0; rt < arr.length; rt++) {
+//     sum += arr[rt];
+//     console.log(sum);
+
+//     if (rt === k - 1) {
+//       if (sum > max) max = sum;
+//       rt = lt;
+//       lt++;
+//       k++;
+//       sum = 0;
+//     }
+//   }
+
+//   answer = max;
+
+//   return answer;
+// }
+
+// let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+// console.log(solution(3, a));
+
+// ---------------------------------------------------
+
+// 2회차 풀이
 function solution(k, arr) {
-  let answer;
+  let answer = 0;
+  let sum = 0;
+
+  for (let i = 0; i < k; i++) sum += arr[i];
+
+  answer = sum;
+
+  for (let i = k; i < arr.length; i++) {
+    sum += arr[i] - arr[i - k];
+    answer = Math.max(answer, sum);
+  }
+
   return answer;
 }
 
-let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
-console.log(solution(3, a));
+let b = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(3, b));
