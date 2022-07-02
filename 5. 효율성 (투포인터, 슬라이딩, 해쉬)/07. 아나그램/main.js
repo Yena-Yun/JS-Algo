c538d377be10315c16b404b1f8bf0540aae9599c
Yena-Yun <cheryleduart@gmail.com>
2022-07-02 16:34:53 +0900

    :bulb: 5-7 주석 수정

--- 5. 효율성 (투포인터, 슬라이딩, 해쉬)/07. 아나그램/main.js
+++ 5. 효율성 (투포인터, 슬라이딩, 해쉬)/07. 아나그램/main.js
@@ -1,33 +1,33 @@
-// // 처음에 푼 코드
-// function solution(str1, str2) {
-//   let answer = 'YES';
-//   let sH = new Map();
-//   let pH = new Map();
+// 처음에 푼 코드
+function solution(str1, str2) {
+  let answer = 'YES';
+  let sH = new Map();
+  let pH = new Map();
 
-//   for (let x of str1) {
-//     if (sH.has(x)) sH.set(x, sH.get(x) + 1);
-//     else sH.set(x, 1);
-//   }
+  for (let x of str1) {
+    if (sH.has(x)) sH.set(x, sH.get(x) + 1);
+    else sH.set(x, 1);
+  }
 
-//   for (let x of str2) {
-//     if (pH.has(x)) pH.set(x, pH.get(x) + 1);
-//     else pH.set(x, 1);
-//   }
+  for (let x of str2) {
+    if (pH.has(x)) pH.set(x, pH.get(x) + 1);
+    else pH.set(x, 1);
+  }
 
-//   for (let [key1, val1] of sH) {
-//     for (let [key2, val2] of pH) {
-//       if (key1 === key2) {
-//         if (val1 === val2) answer = 'YES';
-//       }
-//     }
-//   }
+  for (let [key1, val1] of sH) {
+    for (let [key2, val2] of pH) {
+      if (key1 === key2) {
+        if (val1 === val2) answer = 'YES';
+      }
+    }
+  }
 
-//   return answer;
-// }
+  return answer;
+}
 
-// let a = 'AbaAeCe';
-// let b = 'baeeACA';
-// console.log(solution(a, b));
+let a = 'AbaAeCe';
+let b = 'baeeACA';
+console.log(solution(a, b));
 
 // sH 하나만 써서 다시 푼 코드
 function solution(str1, str2) {
@@ -42,13 +42,13 @@ function solution(str1, str2) {
   console.log(sH);
 
   for (let x of str2) {
-    // 아나그램이 아닌 경우에는 바로 'NO' 반환 (answer에 넣어주는 연산을 굳이 더 할 필요 없음)
+    // 아나그램이 아닌 경우 2가지
     // 1) 2번째 문자열에 해당 문자가 아예 없는 경우
-    // 2) (문자를 만날 때마다 갯수를 빼주다 보니) 해시 테이블에서의 갯수는 0이 된 경우
-    if (!sH.get(x) || sH.get(x) === 0) return 'NO';
+    // 2) (문자를 만날 때마다 갯수를 빼주다가) 해시 테이블에서의 갯수가 0이 된 경우
+    if (!sH.get(x) || sH.get(x) === 0) return 'NO'; // 바로 'NO' 반환 (answer에 넣어주는 연산 생략)
 
-    // str2에서 문자를 찾았다면(let x)
-    // 해시 테이블 쪽에서의 해당 문자의 갯수는 1개를 빼 준다.
+    // (str2에서 문자를 찾은 경우(let x))
+    // 해시 테이블의 해당 문자의 갯수를 1 빼 준다.
     sH.set(x, sH.get(x) - 1);
   }
 
