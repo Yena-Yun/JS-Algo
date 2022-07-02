00284c0d92ab9616e6b156b9c20bee182a328cf1
Yena-Yun <cheryleduart@gmail.com>
2022-07-02 16:28:31 +0900

    :bulb: 5-6 주석 수정

--- 5. 효율성 (투포인터, 슬라이딩, 해쉬)/06. 학급 회장(해쉬)/main.js
+++ 5. 효율성 (투포인터, 슬라이딩, 해쉬)/06. 학급 회장(해쉬)/main.js
@@ -8,7 +8,7 @@ function solution(s) {
   for (let x of s) {
     if (!candidates.includes(x)) {
       candidates.push(x);
-      table[x] = 1; // key로 들어가는 순간 한 번은 호명된 것이므로 1로 초기화
+      table[x] = 1; // key로 들어가는 순간 이름이 한 번은 호명된 것이므로 1로 초기화
     }
 
     table[x] += 1;
@@ -28,8 +28,8 @@ console.log(solution(str));
 // 자료구조 Map을 이용한 방식
 function solution(s) {
   let answer;
-  // Map의 요소는 [key, val] 형식
-  // 메서드: set(key, value), get(key), has(key)
+  // Map의 요소는 [key, val] 형식으로 이루어진다.
+  // Map의 메서드: set(key, value), get(key), has(key)
   let sH = new Map();
 
   for (let x of s) {
