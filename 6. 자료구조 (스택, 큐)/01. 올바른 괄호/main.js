6e3a789355572d34663c5069cdc950a63f25abb8
Yena-Yun <cheryleduart@gmail.com>
2022-07-04 09:45:14 +0900

    :sparkles::memo: 6-1 문제풀이 및 BEFORE, AFTER 작성

--- 6. 자료구조 (스택, 큐)/01. 올바른 괄호/main.js
+++ 6. 자료구조 (스택, 큐)/01. 올바른 괄호/main.js
@@ -1,5 +1,16 @@
 function solution(s) {
-  let answer;
+  let answer = 'YES';
+  let stack = [];
+
+  for (let x of s) {
+    if (x === '(') stack.push(x);
+    else {
+      if (stack.length === 0) return 'NO';
+      stack.pop();
+    }
+  }
+
+  if (stack.length > 0) answer = 'NO';
 
   return answer;
 }
