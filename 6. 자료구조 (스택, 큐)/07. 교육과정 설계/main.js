fae917f1c21874341747a6e648bd0b7d5ccf0fdf
Yena-Yun <cheryleduart@gmail.com>
2022-07-06 16:30:00 +0900

    :sparkles::memo: 6-7 문제풀이 및 BEFORE, AFTER 작성

--- 6. 자료구조 (스택, 큐)/07. 교육과정 설계/main.js
+++ 6. 자료구조 (스택, 큐)/07. 교육과정 설계/main.js
@@ -1,9 +1,21 @@
 function solution(need, plan) {
   let answer = 'YES';
+  let queue = need.split('');
+
+  for (let x of plan) {
+    if (queue.includes(x)) {
+      // if (x === queue[0]) {
+      //   queue.shift();
+      // } else return 'NO';
+      if (x !== queue.shift()) return 'NO';
+    }
+  }
+
+  if (queue.length > 0) return 'NO';
 
   return answer;
 }
 
 let a = 'CBA';
-let b = 'CBDAGE';
+let b = 'CABDGE';
 console.log(solution(a, b));
