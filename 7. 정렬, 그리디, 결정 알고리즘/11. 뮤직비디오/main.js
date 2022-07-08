35a6fdf3bfcbad12a0a7e53abbbb557065380d45
Yena-Yun <cheryleduart@gmail.com>
2022-07-08 21:18:00 +0900

    :package: 챕터7 폴더구조 설정

---
+++ 7. 정렬, 그리디, 결정 알고리즘/11. 뮤직비디오/main.js
@@ -0,0 +1,14 @@
+function count(songs, capacity) {
+  let cnt;
+
+  return cnt;
+}
+
+function solution(m, songs) {
+  let answer;
+
+  return answer;
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(solution(3, arr));
