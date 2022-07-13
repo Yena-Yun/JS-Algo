fd208e8fc359ba98228e84778b60853353d216b4
Yena-Yun <cheryleduart@gmail.com>
2022-07-13 12:51:26 +0900

    :sparkles::memo: 7-3 문제풀이 및 BEFORE, AFTER 작성

--- 7. 정렬, 그리디, 결정 알고리즘/03. Special Sort/main.js
+++ 7. 정렬, 그리디, 결정 알고리즘/03. Special Sort/main.js
@@ -1,8 +1,57 @@
+// 내가 처음에 푼 코드
 function solution(arr) {
   let answer = arr;
+  let queue = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] < 0) {
+      let target = arr.splice(i, 1)[0];
+      queue.push(target);
+    }
+  }
+
+  for (let x of queue) {
+    arr.unshift(x);
+  }
 
   return answer;
 }
 
 let arr = [1, 2, 3, -3, -2, 5, 6, -6];
 console.log(solution(arr));
+
+// 답 코드 참고: 1번째 풀이
+function solution(arr) {
+  let answer = arr;
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > 0 && arr[j + 1] < 0) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return answer;
+}
+
+let arr1 = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr1));
+
+// 답 코드 참고: 2번째 풀이
+function solution(arr) {
+  let answer = [];
+
+  for (let x of arr) {
+    if (x < 0) answer.push(x);
+  }
+
+  for (let x of arr) {
+    if (x > 0) answer.push(x);
+  }
+
+  return answer;
+}
+
+let arr2 = [1, 2, 3, -3, -2, 5, 6, -6];
+console.log(solution(arr2));
