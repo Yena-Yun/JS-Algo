db7c9900beb6d5cb88999f28e4ba310cf7a068a7
Yena-Yun <cheryleduart@gmail.com>
2022-07-18 17:52:32 +0900

    :bulb: 7-3 주석 수정

--- 7. 정렬, 그리디, 결정 알고리즘/03. Special Sort/main.js
+++ 7. 정렬, 그리디, 결정 알고리즘/03. Special Sort/main.js
@@ -23,9 +23,10 @@ console.log(solution(arr));
 // 답 코드 참고: 1번째 풀이
 function solution(arr) {
   let answer = arr;
+  let len = arr.length;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > 0 && arr[j + 1] < 0) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       }
