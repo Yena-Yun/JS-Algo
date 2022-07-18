b9f3de10211eee95297f4671d7e0148833f5259f
Yena-Yun <cheryleduart@gmail.com>
2022-07-18 17:30:43 +0900

    :bulb: 7-2 주석 수정

--- 7. 정렬, 그리디, 결정 알고리즘/02. 버블정렬/main.js
+++ 7. 정렬, 그리디, 결정 알고리즘/02. 버블정렬/main.js
@@ -16,9 +16,10 @@ console.log(solution(arr));
 // 두 번째로 푼 코드
 function solution(arr) {
   let answer = arr;
+  let len = arr.length;
 
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - i - 1; j++) {
       if (arr[j] > arr[j + 1]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
