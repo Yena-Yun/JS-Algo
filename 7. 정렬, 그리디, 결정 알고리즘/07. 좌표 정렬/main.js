7419ccb67e67043db7f8cfa24311cf8cc8cc1683
Yena-Yun <cheryleduart@gmail.com>
2022-07-24 19:40:43 +0900

    :sparkles::memo: 7-7 문제풀이 및 BEFORE, AFTER 작성

--- 7. 정렬, 그리디, 결정 알고리즘/07. 좌표 정렬/main.js
+++ 7. 정렬, 그리디, 결정 알고리즘/07. 좌표 정렬/main.js
@@ -1,7 +1,6 @@
+// 내가 짠 코드
 function solution(arr) {
-  let answer = arr;
-
-  return answer;
+  return arr.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
 }
 
 let arr = [
@@ -12,3 +11,22 @@ let arr = [
   [3, 6],
 ];
 console.log(solution(arr));
+
+// 답 코드
+function solution(arr) {
+  let answer = arr;
+  arr.sort((a, b) => {
+    if (a[0] === b[0]) return a[1] - b[1];
+    else return a[0] - b[0];
+  });
+  return answer;
+}
+
+let arr1 = [
+  [2, 7],
+  [1, 3],
+  [1, 2],
+  [2, 5],
+  [3, 6],
+];
+console.log(solution(arr1));
