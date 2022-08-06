199fcfd75b31abe2d2e7e37bf34c6fd6d6ac9b92
Yena-Yun <cheryleduart@gmail.com>
2022-08-06 21:53:09 +0900

    :sparkles::memo: 7-11 문제풀이 및 BEFORE, AFTER 작성

--- 7. 정렬, 그리디, 결정 알고리즘/11. 뮤직비디오/main.js
+++ 7. 정렬, 그리디, 결정 알고리즘/11. 뮤직비디오/main.js
@@ -1,14 +1,37 @@
 function count(songs, capacity) {
-  let cnt;
+  let cnt = 1;
+  let sum = 0;
+
+  for (let x of songs) {
+    if (sum + x > capacity) {
+      cnt++;
+      sum = x;
+    } else {
+      sum += x;
+    }
+  }
 
   return cnt;
 }
 
 function solution(m, songs) {
-  let answer;
+  let answer = 0;
+  let lt = Math.max(...songs); // songs 중 최대값 =  9
+  let rt = songs.reduce((acc, cur) => acc + cur, 0); // songs 전체 합 = 45
+
+  while (lt <= rt) {
+    let mid = parseInt((lt + rt) / 2);
+
+    if (count(songs, mid) <= m) {
+      answer = mid;
+      rt = mid - 1;
+    } else {
+      lt = mid + 1;
+    }
+  }
 
   return answer;
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(solution(3, arr));
