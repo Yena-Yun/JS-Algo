3de3b415c737f009226a3ed75c17f3f2e0176de8
Yena-Yun <cheryleduart@gmail.com>
2022-08-13 21:28:39 +0900

    :sparkles::memo: 8-1 문제풀이 및 BEFORE, AFTER 로직 작성

--- 8. 재귀함수, 깊이우선탐색(DFS)/01. 재귀함수/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/01. 재귀함수/main.js
@@ -1,5 +1,24 @@
+// 내가 짠 코드
 function solution(n) {
-  function DFS(L) {}
+  function DFS(L) {
+    for (let i = 0; i < L; i++) console.log(i + 1);
+  }
+
+  DFS(n);
+}
+
+solution(3);
+
+// 답 코드 방향
+function solution(n) {
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+      console.log(L);
+    }
+  }
+
   DFS(n);
 }
 
