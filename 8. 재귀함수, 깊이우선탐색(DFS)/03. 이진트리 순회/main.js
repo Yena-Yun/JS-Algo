f0eb699381125d816ca900658c0e8fd6da49dc47
Yena-Yun <cheryleduart@gmail.com>
2022-08-15 22:42:02 +0900

    :sparkles::memo: 8-3 문제풀이 및 BEFORE, AFTER 정리

--- 8. 재귀함수, 깊이우선탐색(DFS)/03. 이진트리 순회/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/03. 이진트리 순회/main.js
@@ -1,5 +1,37 @@
+// 전위 순회
+// function solution(n) {
+//   let answer = '';
+
+//   function DFS(v) {
+//     if (v > 7) return;
+//     else {
+//       answer += v + ' ';
+//       DFS(v * 2);
+//       DFS(v * 2 + 1);
+//     }
+//   }
+
+//   DFS(n);
+
+//   return answer;
+// }
+
+// console.log(solution(1));
+
+// 후위 순회
 function solution(n) {
   let answer = '';
+
+  function DFS(v) {
+    if (v > 7) return;
+    else {
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+      answer += v + ' ';
+    }
+  }
+
+  DFS(n);
 
   return answer;
 }
