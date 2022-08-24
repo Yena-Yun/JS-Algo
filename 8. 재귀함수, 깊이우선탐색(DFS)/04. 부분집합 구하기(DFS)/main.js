8b9768dcc781c82eabe8370fb53206cb7e382245
Yena-Yun <cheryleduart@gmail.com>
2022-08-24 20:08:14 +0900

    :sparkles::memo: 8-4 문제풀이 및 BEFORE, AFTER 작성

--- 8. 재귀함수, 깊이우선탐색(DFS)/04. 부분집합 구하기(DFS)/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/04. 부분집합 구하기(DFS)/main.js
@@ -1,7 +1,65 @@
+// 내가 푼 코드
 function solution(n) {
   let answer = [];
+
+  function DFS(L) {
+    if (L === 0) return;
+    else {
+      DFS(L - 1);
+
+      let arr = [];
+
+      function Sub(L) {
+        if (L === 0) return;
+        else {
+          Sub(L - 1);
+          arr.push(L);
+        }
+      }
+
+      Sub(L);
+      answer.push(arr);
+    }
+  }
+
+  DFS(n);
 
   return answer;
 }
 
-console.log(solution(3));
+console.log(solution(3)); // [ [ 1 ], [ 1, 2 ], [ 1, 2, 3 ] ]
+
+// 답 코드
+function solution(n) {
+  let answer = [];
+  let ch = Array.from({ length: n + 1 }, () => 0);
+
+  // console.log('ch: ' + ch); // [0, 0, 0, 0]
+
+  function DFS(L) {
+    if (L === n + 1) {
+      let tmp = '';
+
+      console.log('ch: ' + ch);
+
+      for (let i = 1; i <= n; i++) {
+        if (ch[i] === 1) tmp += i + ' ';
+      }
+
+      console.log('tmp: ' + tmp);
+
+      if (tmp.length > 0) answer.push(tmp.trim());
+    } else {
+      ch[L] = 1;
+      DFS(L + 1);
+      ch[L] = 0;
+      DFS(L + 1);
+    }
+  }
+
+  DFS(1);
+
+  return answer;
+}
+
+console.log(solution(3)); // [ '1 2 3', '1 2', '1 3', '1', '2 3', '2', '3' ]
