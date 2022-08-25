90d22cc9f136b85446bd1cc6d4e480be6740cc43
Yena-Yun <cheryleduart@gmail.com>
2022-08-25 13:32:36 +0900

    :sparkles::memo: 8-5 문제풀이 및 BEFORE, AFTER 작성

--- 8. 재귀함수, 깊이우선탐색(DFS)/05. 합이 같은 부분집합(DFS, 아마존)/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/05. 합이 같은 부분집합(DFS, 아마존)/main.js
@@ -1,5 +1,56 @@
+// // 내가 푼 코드
+// function solution(arr) {
+//   let answer = 'NO';
+
+//   let seoroso = [];
+//   let counterPart = [];
+
+//   const isPrime = (n) => {
+//     for (let i = 2; i < Math.ceil(Math.sqrt(n)); i++) {
+//       if (n % i === 0) return false;
+//     }
+
+//     return true;
+//   };
+
+//   for (let x of arr) {
+//     if (isPrime(x)) seoroso.push(x);
+//     else counterPart.push(x);
+//   }
+
+//   if (
+//     seoroso.reduce((a, b) => a + b, 0) ===
+//     counterPart.reduce((a, b) => a + b, 0)
+//   )
+//     answer = 'YES';
+
+//   return answer;
+// }
+
+// let arr = [1, 3, 5, 6, 7, 10];
+// console.log(solution(arr));
+
+// 답 코드
 function solution(arr) {
   let answer = 'NO';
+  let flag = 0;
+  let total = arr.reduce((a, b) => a + b, 0);
+  let n = arr.length;
+
+  function DFS(L, sum) {
+    if (flag) return;
+    if (L === n) {
+      if (total - sum === sum) {
+        answer = 'YES';
+        flag = 1;
+      }
+    } else {
+      DFS(L + 1, sum + arr[L]);
+      DFS(L + 1, sum);
+    }
+  }
+
+  DFS(0, 0);
 
   return answer;
 }
