ca4701670b328b792971bfae33a7c7f9117595b2
Yena-Yun <cheryleduart@gmail.com>
2022-08-30 19:50:33 +0900

    :construction: 8-6 일부 문제풀이 코드 추가

--- 8. 재귀함수, 깊이우선탐색(DFS)/06. 바둑이 승차(DFS)/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/06. 바둑이 승차(DFS)/main.js
@@ -1,5 +1,30 @@
 function solution(c, arr) {
-  let answer;
+  let answer = 0;
+
+  // startnode를 구한다.
+  let startNode = arr[0];
+
+  let visitedQueue = []; // 탐색을 마친 노드들
+  let willVisitStack = []; // 탐색해야 할 노드들
+
+  willVisitStack.push(startNode); // 노드 탐색 시작
+
+  // 탐색해야 할 노드가 남아 있다면
+  while (willVisitStack.length !== 0) {
+    // 검사할 노드를 꺼내서
+    const node = willVisitStack.pop();
+    console.log('node: ' + node);
+
+    // 탐색을 마친 노드에 노드가 있는지 검사
+    if (!visitedQueue.includes(node)) {
+      visitedQueue.push(node); // 없으면 추가
+      console.log('visitedQueue: ' + visitedQueue);
+      // console.log('arr[node]: ' + arr[node]);
+      willVisitStack = [...willVisitStack, node];
+    }
+
+    console.log('willVisitStack: ' + willVisitStack);
+  }
 
   return answer;
 }
