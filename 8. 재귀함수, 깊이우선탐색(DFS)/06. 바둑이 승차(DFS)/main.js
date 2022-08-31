499c9fb443ec1949b2af64e4240801e817bfbdae
Yena-Yun <cheryleduart@gmail.com>
2022-08-31 17:57:37 +0900

    :construction::memo: 8-6 문제풀이 일부 진행

--- 8. 재귀함수, 깊이우선탐색(DFS)/06. 바둑이 승차(DFS)/main.js
+++ 8. 재귀함수, 깊이우선탐색(DFS)/06. 바둑이 승차(DFS)/main.js
@@ -1,3 +1,4 @@
+// DFS의 일반적인 코드
 function solution(c, arr) {
   let answer = 0;
 
@@ -31,3 +32,31 @@ function solution(c, arr) {
 
 let arr = [81, 58, 42, 33, 61];
 console.log(solution(259, arr));
+
+// 바둑이 문제 풀기
+function solution(c, arr) {
+  let answer = 0;
+  let weights = []; // 구한 무게들
+  let visited = []; // 탐색을 마친 노드들
+
+  // arr를 정렬한다.
+  arr.sort((a, b) => a - b);
+  console.log(arr); // [ 33, 42, 58, 61, 81 ]
+
+  // 가능한 무게 배열 dogs를 구해 DFS에 전달한다.
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = i + 1; j < arr.length; j++) {}
+  }
+
+  function DFS(dogs) {
+    let sum = dogs.reduce((a, b) => a + b, 0);
+    weights.push(sum);
+  }
+
+  // 요소를 1개씩 뽑는 것은 당연히 가장 무거운 무게가 될 수 없기 때문에 제외한다.
+
+  return answer;
+}
+
+let array = [81, 58, 42, 33, 61];
+console.log(solution(259, array));
